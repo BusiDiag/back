9d8078bcf1bfbf6ccb7b6128b07eccfc12fb0d18
Yeongjin Kim <7591blitzloft@gmail.com>
2024-08-01 20:43:14 +0900

    INIT Project setup

---
+++ src/main/java/com/seeds/busidiag/BusidiagApplication.java
@@ -0,0 +1,13 @@
+package com.seeds.busidiag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusidiagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusidiagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seeds/busidiag/BusidiagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeds.busidiag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusidiagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

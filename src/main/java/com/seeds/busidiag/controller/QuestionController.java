962ff89b8824b662cc0a564b57814bf011f16ea2
Yeongjin Kim <7591blitzloft@gmail.com>
2024-11-14 15:29:52 +0900

    FEAT: Controllers - DiagnosisController implemented - QuestionController implemented - ResponseController implemented - UserController commented as TODO

--- src/main/java/com/seeds/busidiag/controller/QuestionController.java
+++ src/main/java/com/seeds/busidiag/controller/QuestionController.java
@@ -1,4 +1,78 @@
 package com.seeds.busidiag.controller;
 
+import com.seeds.busidiag.entity.Question;
+import com.seeds.busidiag.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/questions")
 public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> createQuestion(@RequestBody Question question) {
+        try {
+            int id = questionService.save(question);
+            return new ResponseEntity<>(id, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Question> getQuestionById(@PathVariable int id) {
+        try {
+            Question question = questionService.findById(id);
+            return new ResponseEntity<>(question, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<Question> getQuestionByCategory(@PathVariable String category) {
+        try {
+            Question question = questionService.findByCategory(category);
+            return new ResponseEntity<>(question, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Integer> updateQuestion(@RequestBody Question question, @PathVariable int id) {
+        try {
+            int updatedId = questionService.updateById(question, id);
+            return new ResponseEntity<>(updatedId, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Integer> deleteQuestion(@PathVariable int id) {
+        try {
+            int deletedId = questionService.deleteById(id);
+            return new ResponseEntity<>(deletedId, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/seeds/busidiag/controller/ResponseController.java
+++ src/main/java/com/seeds/busidiag/controller/ResponseController.java
@@ -1,4 +1,102 @@
 package com.seeds.busidiag.controller;
 
+import com.seeds.busidiag.entity.Response;
+import com.seeds.busidiag.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/responses")
 public class ResponseController {
+
+    private final ResponseService responseService;
+
+    @Autowired
+    public ResponseController(ResponseService responseService) {
+        this.responseService = responseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> createResponse(@RequestBody Response response) {
+        try {
+            int id = responseService.save(response);
+            return new ResponseEntity<>(id, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getResponseById(@PathVariable int id) {
+        try {
+            Response response = responseService.findById(id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/diagnosis/id={id}")
+    public ResponseEntity<Response> getResponseByDiagnosisId(@PathVariable int id) {
+        try {
+            Response response = responseService.findByDiagnosisId(id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/diagnosis/id={diagnosisId}/question/id={questionId}")
+    public ResponseEntity<Response> getResponseByDiagnosisIdAndQuestionId(@PathVariable int diagnosisId, @PathVariable int questionId) {
+        try {
+            Response response = responseService.findByDiagnosisIdAndQuestionId(diagnosisId, questionId);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/diagnosis/id={diagnosisId}/question/id={questionId}/category/id={categoryId}")
+    public ResponseEntity<Response> getResponseByDiagnosisIdAndQuestionIdAndCategoryId(@PathVariable int diagnosisId, @PathVariable int questionId, @PathVariable String categoryId) {
+        try {
+            Response response = responseService.findByDiagnosisIdAndQuestionIdAndCategory(diagnosisId, questionId, categoryId);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Integer> updateResponse(@RequestBody Response question, @PathVariable int id) {
+        try {
+            int updatedId = responseService.updateById(question, id);
+            return new ResponseEntity<>(updatedId, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Integer> deleteResponse(@PathVariable int id) {
+        try {
+            int deletedId = responseService.deleteById(id);
+            return new ResponseEntity<>(deletedId, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
